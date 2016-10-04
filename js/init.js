69d33d7a19902ae1f1bd5927764aeb45229e11ca
John Armstrong <j.armstrong484@gmail.com>
2016-10-04 20:24:26 +1300

    Added a skills collection to hold all skills

--- js/init.js
+++ js/init.js
@@ -1,35 +1,41 @@
-$( document ).ready(function() {
-		$(".typed").typed({
-			strings: ["Software Engineer", "Web Developer", "Programmer"], // ^{number} denotes time in ms to wait
+$(document).ready(function () {
+	$(".typed").typed({
+		strings: ["Software Engineer", "Web Developer", "Programmer"], // ^{number} denotes time in ms to wait
 
-			// typing speed
-			typeSpeed: 100,
+		// typing speed
+		typeSpeed: 100,
 
-			// time before typing starts
-			startDelay: 500,
+		// time before typing starts
+		startDelay: 500,
 
-			// backspacing speed
-			backSpeed: 20,
+		// backspacing speed
+		backSpeed: 20,
 
-			// time before backspacing
-			backDelay: 2000,
+		// time before backspacing
+		backDelay: 2000,
 
-			// loop
-			loop: true,
+		// loop
+		loop: true,
 
-			// false = infinite
-			loopCount: false,
+		// false = infinite
+		loopCount: false,
 
-			// show cursor
-			showCursor: true,
+		// show cursor
+		showCursor: true,
 
-			// character for cursor
-			cursorChar: "",
+		// character for cursor
+		cursorChar: "",
 
-			// attribute to type (null == text)
-			attr: null,
+		// attribute to type (null == text)
+		attr: null,
 
-			// either html or text
-			contentType: 'html',
-		});
+		// either html or text
+		contentType: 'html'
+	});
+
+	$('.skills').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3
+	});
 });
